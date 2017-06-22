bf0715c2840e6a69ed94cdd5a29fad402371894d
Miguel Angel Ardon Enriquez <miguel.ardon@unitec.edu>
2017-06-21 20:03:10 -0600

    arreglando arbol

--- BTreeNode.cc
+++ BTreeNode.cc
@@ -22,3 +22,40 @@ RC BTLeafNode::write(PageId pid, PageFile& pf){
     
     return pf.write(pid, buffer);
 }
+
+int BTLeafNode::getKeyCount(){   
+    int keyCounter=0;
+    char* tmp=buffer;
+ 
+    int curr;
+    int i=4;
+
+    while(i<1024){
+        memcpy(&curr,tmp,sizeof(int));
+        if(curr==0)
+            break;
+        
+        keyCounter++;
+        tmp+=L_PAIR_SIZE;
+        i+=L_PAIR_SIZE;
+    }
+
+    if(keyCounter>1){
+        memcpy(&curr,tmp-sizeof(int),sizeof(int));
+        if(curr==0){
+            memcpy(&curr,tmp-2*sizeof(int),sizeof(int));
+            if(curr==0){
+                keyCounter--;
+            }
+        }
+    }
+
+    return keyCounter;
+}
+
+
+int BTLeafNode::getMaxLlaves(){
+    
+    int parejas_maximas=floor((PageFile::PAGE_SIZE-sizeof(PageId))/(L_PAIR_SIZE));
+    return parejas_maximas;
+}
